a9aa4fbdaeb5cc286df863d3740e45cc1e735919
Matteo Sist <matteo.sist@molo17.com>
2019-07-04 18:23:06 +0200

    add maven plugin
    fix missing plugins

--- android/src/main/java/com/molo17/commons/android/FragmentExtensions.kt
+++ android/src/main/java/com/molo17/commons/android/FragmentExtensions.kt
@@ -1,9 +1,9 @@
 package com.molo17.commons.android
 
-import android.app.Activity
 import android.view.ViewGroup
 import androidx.appcompat.widget.ActionMenuView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -18,13 +18,10 @@ inline fun <reified VM : ViewModel> Fragment.getFragmentViewModel(): VM =
     ViewModelProviders.of(this, (requireActivity() as HasViewModelFactory).viewModelFactory).get(VM::class.java)
 
 inline fun <reified VM : ViewModel> Fragment.getActivityViewModel(): VM =
-    (requireActivity() as HasViewModelFactory).run {
-        ViewModelProviders.of(this, viewModelFactory).get(VM::class.java)
-    }
+    requireActivity().run { ViewModelProviders.of(this, (this as HasViewModelFactory).viewModelFactory).get(VM::class.java) }
 
-inline fun <reified VM : ViewModel> Activity.getViewModel(): VM = (this as HasViewModelFactory).run {
-    ViewModelProviders.of(this, viewModelFactory).get()
-}
+inline fun <reified VM : ViewModel> FragmentActivity.getViewModel(): VM =
+    ViewModelProviders.of(this, (this as HasViewModelFactory).viewModelFactory).get(VM::class.java)
 
 val Fragment.actionMenuView: ActionMenuView?
     get() {
