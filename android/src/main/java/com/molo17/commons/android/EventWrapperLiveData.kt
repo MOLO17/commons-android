c658aa4360c6c3a79f485ee8fe219701876d9afe
Federico Monti <federico.monti@molo17.com>
2019-12-05 17:19:30 +0100

    Added handling of observer remove

--- android/src/main/java/com/molo17/commons/android/EventWrapperLiveData.kt
+++ android/src/main/java/com/molo17/commons/android/EventWrapperLiveData.kt
@@ -23,6 +23,9 @@ class EventLiveData<T> : MutableLiveData<Event<T>>()
 fun <T> MutableLiveData<Event<T>>.postEvent(value: T) = postValue(Event(value))
 
 fun <T> LiveData<Event<T>>.unwrapEvent(): LiveData<T> = object : LiveData<T>() {
+
+    private val retainedObservers = mutableMapOf<Observer<in T>, Observer<in Event<T>>>()
+
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
         this@unwrapEvent.observe(owner, Observer { event ->
             event?.popValue()?.also(observer::onChanged)
@@ -30,8 +33,17 @@ fun <T> LiveData<Event<T>>.unwrapEvent(): LiveData<T> = object : LiveData<T>() {
     }
 
     override fun observeForever(observer: Observer<in T>) {
-        this@unwrapEvent.observeForever { event ->
+        val wrappedObserver = Observer<Event<T>> { event ->
             event?.popValue()?.also(observer::onChanged)
+        }
+        retainedObservers[observer] = wrappedObserver
+        this@unwrapEvent.observeForever(wrappedObserver)
+    }
+
+    override fun removeObserver(observer: Observer<in T>) {
+        retainedObservers[observer]?.let { retained ->
+            this@unwrapEvent.removeObserver(retained)
+            retainedObservers.remove(observer)
         }
     }
 }
